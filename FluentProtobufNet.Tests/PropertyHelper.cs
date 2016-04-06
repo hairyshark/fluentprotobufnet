7152f5aa03970770fd05c25d9e2770a7cace7c0d
UncleBook <hairyshark@users.noreply.github.com>
2016-04-06 09:48:43 +0100

    Update PropertyHelper.cs

--- FluentProtobufNet.Tests/PropertyHelper.cs
+++ FluentProtobufNet.Tests/PropertyHelper.cs
@@ -1,24 +1,30 @@
-using System;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.Serialization;
-using System.Text;
-using FluentProtobufNet.Helpers;
-
 namespace FluentProtobufNet.Tests
 {
+    using System;
+    using System.Reflection;
+    using System.Text;
+
+    using NUnit.Framework;
+
     public static class PropertyHelper
     {
-        public static string Properties(this object item)
+        public const BindingFlags defaultFlags = BindingFlags.Public | BindingFlags.GetProperty | BindingFlags.Instance;
+
+        public static string Properties(this object item, BindingFlags flags = defaultFlags)
         {
             var sb = new StringBuilder();
 
-            foreach (var propertyInfo in item.GetType().GetProperties())
+            foreach (var propertyInfo in item.GetType().GetProperties(flags))
             {
                 sb.Append(string.Format("{0}={1};", propertyInfo.Name, propertyInfo.GetMethod.Invoke(item, null)));
             }
 
             return sb.ToString();
+        }
+
+        public static void WithPropertyCount(this ModelDescriptor descriptor, int expected, BindingFlags flags = BindingFlags.Public | BindingFlags.Instance)
+        {
+            Assert.AreEqual(expected, descriptor.TypeSource.GetProperties(flags).Length);
         }
     }
 }
